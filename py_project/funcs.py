450f1564330fe24b50599defcbedca89000efd97
Tvgenii Ermak <djermak3000@mail.ru>
2023-09-13 13:48:52 +0300

    Fix funcs.py

--- py_project/funcs.py
+++ py_project/funcs.py
@@ -4,3 +4,11 @@ def addition():
     number_2 = int(input())
     answer = number_1 + number_2
     return answer
+
+
+def subtraction():
+    """Функция вычитания двух чисел"""
+    number_1 = int(input())
+    number_2 = int(input())
+    answer_2 = number_1 - number_2
+    return answer_2
